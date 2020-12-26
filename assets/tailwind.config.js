a561eda0e0dece0ba82eb7d16111167e40d6f44b
Ben Smith <benedictsmith@gmail.com>
2020-12-26 11:19:01 +0100

    Add Kutty

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#6B7280", // For lighter primary color
+          DEFAULT: "#1F2937", // Normal primary color
+          dark: "#111827", // Used for hover, active, etc.
+        },
+      },
+    },
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require("kutty")
   ],
 }
