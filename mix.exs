142bd122fd4e45ead1d1ce5943f81b800a48f877
Ben Smith <benedictsmith@gmail.com>
2021-01-06 20:26:18 +0100

    Start SSL app

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Eatbeep.MixProject do
   def application do
     [
       mod: {Eatbeep.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
