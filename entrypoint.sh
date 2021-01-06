874bc082c7c37601fa9a57c4bf64898fe07e962d
Ben Smith <benedictsmith@gmail.com>
2021-01-06 19:43:35 +0100

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+# File: entrypoint.sh
+#!/bin/bash
+# docker entrypoint script.
+
+# assign a default for the database_user
+# DB_USER=${DATABASE_USER:-postgres}
+
+# # wait until Postgres is ready
+# while ! pg_isready -q -h $DATABASE_HOST -p 5432 -U $DB_USER
+# do
+#   echo "$(date) - waiting for database to start"
+#   sleep 2
+# done
+
+bin="/app/bin/eatbeep"
+eval "$bin eval \"Eatbeep.Release.migrate\""
+# start the elixir application
+exec "$bin" "start"
