b7277a21c3c3503865ee4fd3a616daae3d0a93ab
Ben Smith <benedictsmith@gmail.com>
2021-01-11 00:23:21 +0100

    Background images

--- lib/eatbeep_web/live/editor/Hero.ex
+++ lib/eatbeep_web/live/editor/Hero.ex
@@ -6,11 +6,13 @@ defmodule EatbeepWeb.Menu.Hero do
   prop block, :any
 
   def render(assigns) do
+    unsplash = Map.get(assigns.block, :background, "h0Vxgz5tyXA")
+    background_url = "url(https://source.unsplash.com/#{unsplash}/1400x400)"
 
     ~H"""
-    <section class="container px-4 py-32 mx-auto">
+    <section class="px-4 py-32 mx-auto bg-center bg-no-repeat" style={{ background: background_url }}>
       <div class="w-full mx-auto md:w-11/12 xl:w-8/12 text-center">
-        <h1 class="mb-3 text-4xl font-bold text-gray-900 md:text-5xl md:leading-tight md:font-extrabold">{{ @block.name }}</h1>
+        <h1 class="mb-3 text-3xl font-bold text-white md:text-6xl md:leading-tight md:font-extrabold">{{ @block.name }}</h1>
       </div>
     </section>
     <Dialog title="Edit hero" id={{ @block.bid }}>
--- lib/eatbeep_web/live/editor/HeroForm.ex
+++ lib/eatbeep_web/live/editor/HeroForm.ex
@@ -1,11 +1,13 @@
 defmodule EatbeepWeb.Menu.HeroForm do
   use Surface.LiveComponent
   alias Surface.Components.Form
-  alias Surface.Components.Form.{Field, Label, TextInput, Submit, ErrorTag, HiddenInput}
+  alias Surface.Components.Form.{Field, Label, TextInput, Submit, ErrorTag, HiddenInput, RadioButton}
   alias Eatbeep.Blocks
 
   data changeset, :any
   prop block, :any
+
+  @pictures ~w(h0Vxgz5tyXA wKTF65TcReY pJadQetzTkI)a
 
   def update(assigns, socket) do
     cset = Blocks.hero_changeset(assigns.block)
@@ -13,6 +15,7 @@ defmodule EatbeepWeb.Menu.HeroForm do
   end
 
   def render(assigns) do
+    pictures = @pictures
     ~H"""
     <Form submit="submit" for={{ @changeset }} as="block">
       <HiddenInput field="__type" />
@@ -23,6 +26,17 @@ defmodule EatbeepWeb.Menu.HeroForm do
           <TextInput class="input" opts={{ placeholder: "eg. Acme Pizza"}} />
           <ErrorTag class="field-error" />
         </Field>
+        <Field name="background">
+        <div class="grid grid-cols-3 gap-x-4">
+          <label :for={{ picture <- pictures }} class="border p-2 rounded block">
+            <div class="h-32 bg-center bg-no-repeat" style={{ background: "url(#{"https://source.unsplash.com/#{picture}/200x130"}"}} />
+            <div class="flex justify-center p-3">
+            <RadioButton value={{ picture }} />
+            </div>
+          </label>
+
+        </div>
+      </Field>
         <Submit class="btn btn-primary">Update</Submit>
       </div>
       </Form>
@@ -30,7 +44,7 @@ defmodule EatbeepWeb.Menu.HeroForm do
   end
 
    def handle_event("submit", %{"block" => params}, socket) do
-     cset = Blocks.hero_changeset(params) |> Map.put(:action, :update)
+     cset = Blocks.hero_changeset(params) |> Map.put(:action, :update) |> IO.inspect(label: "changeset")
      if cset.valid? do
       send(self(), {:update_block, cset.changes})
       {:noreply, socket}
@@ -39,3 +53,6 @@ defmodule EatbeepWeb.Menu.HeroForm do
      end
    end
 end
+
+# <RadioButton class="h-32 bg-center bg-no-repeat" style={{ background: "url(#{"https://source.unsplash.com/wKTF65TcReY/200x130"}"}} />
+# <RadioButton class="h-32 bg-center bg-no-repeat" style={{ background: "url(#{"https://source.unsplash.com/pJadQetzTkI/200x130"}"}} />
