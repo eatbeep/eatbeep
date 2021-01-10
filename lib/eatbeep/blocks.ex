607ba0df96d6923c4328208b6203237b98bf8a3a
Ben Smith <benedictsmith@gmail.com>
2021-01-11 00:36:42 +0100

    Add starting data

--- lib/eatbeep/blocks.ex
+++ lib/eatbeep/blocks.ex
@@ -4,7 +4,7 @@ defmodule Eatbeep.Blocks do
 
   def hero_changeset(attrs \\ %{}) do
     data = %{}
-    types = %{name: :string, bid: :string, __type: :string}
+    types = %{name: :string, bid: :string, __type: :string, background: :string}
 
       {data, types}
       |> cast(attrs, Map.keys(types))
@@ -44,6 +44,22 @@ defmodule Eatbeep.Blocks do
       name: "Pellentesque ac fermentum metus",
       description: "Vivamus scelerisque nunc ac fermentum euismod. Vestibulum accumsan volutpat lectus."
     }) |> Map.get(:changes)
+  end
+
+  def generate_starting_data() do
+    [%{__type: "hero", bid: Ecto.UUID.generate(), name: "Acme Pizza", background: "h0Vxgz5tyXA"},
+    %{__type: "heading", bid: Ecto.UUID.generate(), heading: "Starters"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Bruschetta", description: "Sun blushed tomatoes, with basil, garlic and olive oil on our homemade bread"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Calamari Fritti", description: "Deep fried squid in a light and crispy batter"},
+    %{__type: "heading", bid: Ecto.UUID.generate(), heading: "Mains"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Calzone Pollo", description: "Folded pizza with chicken breast pieces, button mushrooms and black olives, served with our tomato sauce"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Margherita", description: "Our tomato sauce and mozzarella. Gluten free base available"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Quattro Stagioni", description: "Spicy Italian salami, honey roasted ham, button mushrooms and black olives"},
+    %{__type: "heading", bid: Ecto.UUID.generate(), heading: "Desserts"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Tiramisu", description: "Our very own homemade Tiramisu, lovingly prepared with Savoiardi biscuits soaked in espresso coffee, doused with liquer and topped with mascarpone cream"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Profiteroles", description: "Cream filled choux pastry puffs coated in thick chocolate fondant"},
+    %{__type: "product", bid: Ecto.UUID.generate(), name: "Affogato", description: "Two scoops of Italian ice cream of your choice drowned in espresso coffee"}
+  ]
   end
 
 end
--- lib/eatbeep/signup.ex
+++ lib/eatbeep/signup.ex
@@ -2,10 +2,14 @@ defmodule Eatbeep.Signup do
   alias Ecto.Multi
   alias Eatbeep.Repo
   alias Eatbeep.Login
+  alias Ecto.Changeset
 
   def signup(changeset) do
+
+    with_blocks = changeset |> Changeset.put_change(:blocks, Eatbeep.Blocks.generate_starting_data())
+
     Multi.new()
-    |> Multi.insert(:tenant, changeset)
+    |> Multi.insert(:tenant, with_blocks)
     |> Multi.run(:token, fn _repo, %{tenant: tenant} ->
       Login.generate_login_token(tenant)
     end)
